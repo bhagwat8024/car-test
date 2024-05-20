63ef0006fb3b75166ac1e84932adbc4703998779
Bhagwat Singh <bhagwat.singh@nagarro.com>
2024-05-20 12:58:28 +0530

    initial commit

---
+++ CarTest/CarTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarTestApp.swift
+//  CarTest
+//
+//  Created by Bhagwat Singh on 20/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CarTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
